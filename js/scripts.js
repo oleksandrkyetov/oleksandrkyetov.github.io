9bcf7808a0492c871293f7d98dc4166d2dd6dc56
Oleksandr <oleksandr.kyetov@gmail.com>
2015-04-23 07:31:20 +0300

    release version commit

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var INTERVAL = -1;
 
 // Default options
 var OPTIONS = {
-	'_id': 'options',
+	'_id': OPTIONS_KEY,
 	'gym': {
 		'next': {
 			'hour': 19,
@@ -21,34 +21,53 @@ var OPTIONS = {
 	}
 };
 
-// Options storage
+// Default events
+var EVENTS = {
+	'_id': EVENTS_KEY,
+	'items': []
+};
+
+// Options
 var options = {};
 
-// Events storage
-var events = {};
+// Events
+var events = [];
 
 $(document).ready(function() {
 	var saveOptionsLink = $('#saveOptionsLink');
 	var gymTimeInput = $('#gymTimeInput');
 	var drinkTimeInput = $('#drinkTimeInput');
+
 	var saveSuccessPopup = $('.save.success.popup');
 	var saveFailPopup = $('.save.fail.popup');
-	var timersPopup = $('.timers.popup');
+
+	var addEventLink = $('#addEventLink');
+	var eventNameInput = $('#eventNameInput');
+	var eventDateInput = $('#eventDateInput');
+
+	var addSuccessPopup = $('.add.success.popup');
+	var addFailPopup = $('.add.fail.popup');
 
 	var gymTimer = $('#main .gym.timer');
 	var drinkTimer = $('#main .drink.timer');
+	var timersPopup = $('.timers.popup');
 
 	var eventName = $('#main .event.name');
 	var eventTimer = $('#main .event.timer');
 
-	// Events
-	var toEvent = function() {
-		if (events.length) {
+	var eventsHolder = $('#eventsHolder');
+	var eventTemplate = $('#eventTemplate');
 
+	var toEvent = function() {
+		/*
+		if (events.items && events.items.length) {
+			eventName.find('.name').html(events.items[0].name);
+			eventTimer.find('.date').html(moment(events.items[0].date).format('MM-DD-YYYY HH:mm:ss'));
 		} else {
-			eventName.find('.text').html('Nothing is scheduled yet ...');
-			eventTimer.find('.time').html('--:--:--');
+			eventName.find('.name').html('Nothing is scheduled yet ...');
+			eventTimer.find('.date').html('--:--:--');
 		}
+		*/
 	};
 
 	var toDo = function() {
@@ -63,14 +82,14 @@ $(document).ready(function() {
 			.minute(options.drink.next.minute)
 			.second(options.gym.next.second);
 
-		if (nextGym.isBefore(current)) {
+		while (nextGym.isBefore(current)) {
 			// Add additional day if current is less than next one
 			nextGym.add(moment.duration({
 				'days' : 1
 			}));
 		}
 
-		if (nextDrink.isBefore(current)) {
+		while (nextDrink.isBefore(current)) {
 			// Add additional period if current is less than next one
 			nextDrink.add(moment.duration({
 				'minutes': options.drink.next.minute
@@ -87,10 +106,36 @@ $(document).ready(function() {
 			timersPopupMessage.html(timersPopupMessage.html() + '<div>It is time to drink</div>');
 		}
 
+		if (events.items && events.items.length) {
+			var nextEvent = moment(events.items[0].date);
+			var diffEventDays = nextEvent.diff(current, 'days');
+
+			eventName.find('.name').html(events.items[0].name + ' in');
+
+			if (diffEventDays) {
+				eventTimer.find('.date').html(diffEventDays + ' Days');
+			} else {
+				var diffEvent = nextEvent.diff(current);
+				if (diffEvent < 100) {
+					timersPopupMessage.html(timersPopupMessage.html() + '<div>It is time for ' + events.items[0].name + '</div>');
+
+					// Filter events (remove events in the past)
+					events.items = events.items.filter(function(event) {
+						return moment(event.date).isAfter(moment());
+					});
+				}
+
+				eventTimer.find('.date').html(moment.utc(diffEvent).format('HH:mm:ss'));
+			}
+		} else {
+			eventName.find('.name').html('Nothing is scheduled yet ...');
+			eventTimer.find('.date').html('--:--:--');
+		}
+
 		gymTimer.find('.time').html(moment.utc(diffGym).format('HH:mm:ss'));
 		drinkTimer.find('.time').html(moment.utc(diffDrink).format('HH:mm:ss'));
 
-		if (timersPopupMessage.html().length > 0) {
+		if (timersPopupMessage.html().length) {
 			timersPopup.popup('open', {});
 			clearInterval(INTERVAL);
 		}
@@ -111,6 +156,26 @@ $(document).ready(function() {
 		drinkTimeInput.val(options.drink.next.minute);
 	};
 
+	var updateEvens = function() {
+		// Clear children
+		eventsHolder.children().remove();
+
+		// Add children
+		if (events.items && events.items.length) {
+			events.items.forEach(function(item) {
+				var event = eventTemplate.children().clone();
+				event.find('.name').html(item.name);
+				event.find('.date').html(moment(item.date).format('MM-DD-YYYY HH:mm:ss'));
+				event.appendTo(eventsHolder);
+			});
+		} else {
+			var event = eventTemplate.children().clone();
+			event.find('.name').html('Nothing is scheduled yet ...');
+			event.find('.date').html('MM-DD-YYYY HH:mm:ss');
+			event.appendTo(eventsHolder);
+		}
+	};
+
 	// Check if options is already in database
 	DB.get(OPTIONS_KEY, function(err, doc) {
 		if (err) {
@@ -126,6 +191,14 @@ $(document).ready(function() {
 		} else {
 			options = doc;
 
+			// Trigger options update
+			updateOptions();
+
+			// Trigger options update on page change
+			$(document).on('pagecontainerchange', function(event, ui) {
+				updateOptions();
+			});
+
 			// Start timers
 			count();
 		}
@@ -135,8 +208,34 @@ $(document).ready(function() {
 	DB.get(EVENTS_KEY, function(err, doc) {
 		if (err) {
 			console.log('There is errors getting events from PouchDB ...');
+
+			// Put default options in database
+			DB.put(EVENTS, function callback(err, doc) {
+				if (err) {
+					console.log('There is errors storing options to PouchDB ...');
+				}
+				events = EVENTS;
+			});
 		} else {
 			events = doc;
+
+			// Filter events (remove events in the past)
+			events.items = events.items.filter(function(event) {
+				return moment(event.date).isAfter(moment());
+			});
+
+			// Sort events
+			events.items.sort(function(event1, event2) {
+				return moment(event1.date).isAfter(moment(event2.date));
+			});
+
+			// Trigger events update
+			updateEvens();
+
+			// Trigger events update on page change
+			$(document).on('pagecontainerchange', function(event, ui) {
+				updateEvens();
+			});
 		}
 	});
 
@@ -148,39 +247,91 @@ $(document).ready(function() {
 			return;
 		}
 
-		if (!moment(drinkTimeInput.val(), 'mm', true).isValid()) {
+		if (!moment(drinkTimeInput.val(), 'mm', true).isValid() && !moment(drinkTimeInput.val(), 'm', true).isValid()) {
 			saveFailPopup.find('span').html('Drink time format is incorrect ...');
 			saveFailPopup.popup('open', {});
 			return;
 		}
 
-		var gymVal = moment(gymTimeInput.val(), 'HH:mm:ss');
+		var gymTimeVal = moment(gymTimeInput.val(), 'HH:mm:ss');
 		options.gym.next = {
-			'hour': gymVal.hour(),
-			'minute': gymVal.minute(),
-			'second': gymVal.second()
+			'hour': gymTimeVal.hour(),
+			'minute': gymTimeVal.minute(),
+			'second': gymTimeVal.second()
 		};
-		var drinkVal = moment(drinkTimeInput.val(), 'mm');
+		var drinkMinuteVal = moment(drinkTimeInput.val(), 'mm');
 		options.drink.next = {
-			'minute': drinkVal.minute()
+			'minute': drinkMinuteVal.minute()
 		};
 
-		// Save options
+		// Update options
 		DB.put(options, function callback(err, doc) {
 			if (err) {
 				saveFailPopup.find('span').html('Error when store options ...');
 				saveFailPopup.popup('open', {});
-			}
+			} else {
+				// Update document revision
+				options._rev = doc.rev;
 
-			// Update document revision
-			options._rev = doc.rev;
-			saveSuccessPopup.popup('open', {});
+				saveSuccessPopup.popup('open', {});
+			}
 		});
 	});
 
-	// Trigger options update on page change
-	$(document).on('pagecontainerchange', function(event, ui) {
-		updateOptions();
+	addEventLink.on('click', function() {
+		if (!eventNameInput.val().length) {
+			addFailPopup.find('span').html('Event name cannot be blank ...');
+			addFailPopup.popup('open', {});
+			return;
+		}
+
+		if (!moment(eventDateInput.val(), 'MM-DD-YYYY HH:mm:ss', true).isValid()) {
+			addFailPopup.find('span').html('Event date format is incorrect ...');
+			addFailPopup.popup('open', {});
+			return;
+		}
+
+		if (moment(eventDateInput.val(), 'MM-DD-YYYY HH:mm:ss').isBefore(moment())) {
+			addFailPopup.find('span').html('Event date cannot be in the past ...');
+			addFailPopup.popup('open', {});
+			return;
+		}
+
+		var eventDateVal = moment(eventDateInput.val(), 'MM-DD-YYYY HH:mm:ss');
+		// Add event to the list of events
+		events.items.push({
+			'name': eventNameInput.val(),
+			'date': eventDateVal.toDate()
+		});
+
+		// Filter events (remove events in the past)
+		events.items = events.items.filter(function(event) {
+			return moment(event.date).isAfter(moment());
+		});
+
+		// Sort events
+		events.items.sort(function(event1, event2) {
+			return moment(event1.date).isAfter(moment(event2.date));
+		});
+
+		// Update events
+		DB.put(events, function callback(err, doc) {
+			if (err) {
+				addFailPopup.find('span').html('Error when store events ...');
+				addFailPopup.popup('open', {});
+			} else {
+				// Update document revision
+				events._rev = doc.rev;
+
+				// Clear values
+				eventNameInput.val('');
+				eventDateInput.val('');
+
+				// Update events list on update
+				updateEvens();
+				addSuccessPopup.popup('open', {});
+			}
+		});
 	});
 
 	// Resume count on popup close
