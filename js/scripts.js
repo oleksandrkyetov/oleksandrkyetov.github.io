3bc26ef10c727d7b5e1eaac26380041c8de9f641
Oleksandr <oleksandr.kyetov@gmail.com>
2015-04-24 00:27:44 +0300

    Initial commit for BMI

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,336 @@
 var OPTIONS_KEY = 'options';
-var EVENTS_KEY = 'events';
-var DB = new PouchDB('events');
-var INTERVAL = -1;
+var LOGGED_KEY = 'logged';
+var FOOD_KEY = 'food';
+var DB = new PouchDB('calculator');
+//DB.destroy();
 
 // Default options
 var OPTIONS = {
 	'_id': OPTIONS_KEY,
-	'gym': {
-		'next': {
-			'hour': 19,
-			'minute': 0,
-			'second': 0
-		}
-	},
-	'drink': {
-		'next': {
-			'minute': 30,
-			'second': 0
-		}
-	}
+	'???': {}
 };
 
-// Default events
-var EVENTS = {
-	'_id': EVENTS_KEY,
+// Default logged food
+var LOGGED = {
+	'_id': LOGGED_KEY,
+	'items': []
+};
+
+// Default food database
+var FOOD = {
+	'_id': FOOD_KEY,
 	'items': []
 };
 
 // Options
 var options = {};
 
-// Events
-var events = [];
+// Logged food
+var logged = [];
+
+// Database food
+var food = [];
 
 $(document).ready(function() {
+	var logsHolder = $('#logsHolder');
+	var logTemplate = $('#logTemplate');
+	var logTemplateFailure = $('#logTemplateFailure');
+	var logAddLink = $('#logAddLink');
+	var logFoodNameSelect = $('#logFoodNameSelect');
+	var logFoodNameSpan = $('#logFoodNameSpan');
+	var logFoodDateInput = $('#logFoodDateInput');
+	var logFoodTimeInput = $('#logFoodTimeInput');
+	var foodLogPopupSuccess = $('.food.log.popup.success');
+	var foodLogPopupFail = $('.food.log.popup.fail');
+
+	var foodHolder = $('#foodHolder');
+	var foodTemplate = $('#foodTemplate');
+	var foodTemplateFailure = $('#foodTemplateFailure');
+	var foodAddLink = $('#foodAddLink');
+	var foodNameInput = $('#foodNameInput');
+	var foodCaloriesInput = $('#foodCaloriesInput');
+	var foodAddPopupSuccess = $('.food.add.popup.success');
+	var foodAddPopupFail = $('.food.add.popup.fail');
+
+	var weightInput = $('#weightInput');
+	var heightInput = $('#heightInput');
+	var calculateLink = $('#calculateLink');
+	var bmiPopupSuccess = $('.bmi.popup.success');
+	var bmiPopupFail = $('.bmi.popup.fail');
+	var bmiSpan = $('#bmiSpan');
+
+	var updateLogSelect = function() {
+		logFoodNameSelect.children().remove();
+
+		if (food.items && food.items.length) {
+			logFoodNameSelect.closest('.ui-select').removeClass('hidden');
+			logFoodNameSpan.addClass('hidden');
+
+			logFoodNameSelect.append('<option class="readonly">Select food from list ...</option>', {});
+			food.items.forEach(function(item) {
+				logFoodNameSelect.append('<option>' + item.name + '</option>', {});
+			});
+
+			logFoodNameSelect.trigger('change');
+		} else {
+			logFoodNameSelect.closest('.ui-select').addClass('hidden');
+			logFoodNameSpan.removeClass('hidden');
+		}
+	};
+
+	var updateLogItems = function() {
+		// Clear children
+		logsHolder.children().remove();
+
+		// Add children
+		if (logged.items && logged.items.length) {
+			logged.items.forEach(function(item) {
+				var logItem = logTemplate.children().clone();
+				logItem.find('.name').html(item.name);
+				logItem.find('.dateTime').html(moment(item.dateTime).format('MM-DD-YYYY HH:mm'));
+				logItem.appendTo(logsHolder);
+			});
+		} else {
+			var logItem = logTemplateFailure.children().clone();
+			logItem.html('Nothing is logged yet ...');
+			logItem.appendTo(logsHolder);
+		}
+	};
+
+	var updateFoodItems = function() {
+		// Clear children
+		foodHolder.children().remove();
+
+		// Add children
+		if (food.items && food.items.length) {
+			food.items.forEach(function(item) {
+				var foodItem = foodTemplate.children().clone();
+				foodItem.find('.name').html(item.name);
+				foodItem.find('.calories').html(item.calories);
+				foodItem.appendTo(foodHolder);
+			});
+		} else {
+			var foodItem = foodTemplateFailure.children().clone();
+			foodItem.html('Nothing is added yet ...');
+			foodItem.appendTo(foodHolder);
+		}
+	};
+
+	logAddLink.on('click', function() {
+		if (!logFoodNameSelect.find('option:selected').index()) {
+			foodLogPopupFail.find('span').html('Select food please ...');
+			foodLogPopupFail.popup('open', {});
+			return;
+		}
+
+		if (!logFoodDateInput.val().length && !moment(logFoodDateInput.val(), 'MM-DD-YYY', true).isValid()) {
+			foodLogPopupFail.find('span').html('Date format is invalid ...');
+			foodLogPopupFail.popup('open', {});
+			return;
+		}
+
+		if (!logFoodTimeInput.val().length && !moment(logFoodTimeInput.val(), 'HH:mm', true).isValid()) {
+			foodLogPopupFail.find('span').html('Food name cannot be blank ...');
+			foodLogPopupFail.popup('open', {});
+			return;
+		}
+
+		// Get dates
+		var logFoodDateTimeVal = moment(logFoodDateInput.val() + ' ' + logFoodTimeInput.val(), 'MM-DD-YYYY HH:mm');
+		// Update logged food object
+		logged.items.push({
+			'name': logFoodNameSelect.val(),
+			'dateTime': logFoodDateTimeVal.toDate()
+		});
+
+		// Update food
+		DB.put(logged, function callback(err, doc) {
+			if (err) {
+				foodLogPopupFail.find('span').html('Error when log food ...');
+				foodLogPopupFail.popup('open', {});
+			} else {
+				// Update document revision
+				logged._rev = doc.rev;
+
+				// Sort logged food
+				logged.items.sort(function(logged1, logged2) {
+					return moment(logged1.dateTime).isBefore(moment(logged2.dateTime));
+				});
+
+				// Clear values
+				logFoodNameSelect.find('option:selected').prop('selected', false);
+				logFoodNameSelect.find('option:first').prop('selected', 'selected');
+				logFoodNameSelect.trigger('change');
+
+				logFoodDateInput.trigger('datebox', {'method':'set', 'value': moment().toDate()});
+				logFoodTimeInput.trigger('datebox', {'method':'set', 'value': moment().toDate()});
+
+				// Update corresponding data on success
+				updateLogItems();
+
+				// Success popup
+				foodLogPopupSuccess.popup('open', {});
+			}
+		});
+	});
+
+	foodAddLink.on('click', function() {
+		if (!foodNameInput.val().length) {
+			foodAddPopupFail.find('span').html('Food name cannot be blank ...');
+			foodAddPopupFail.popup('open', {});
+			return;
+		}
+
+		if (!foodCaloriesInput.val().length) {
+			foodAddPopupFail.find('span').html('Food calories cannot be blank ...');
+			foodAddPopupFail.popup('open', {});
+			return;
+		}
+
+		// Update food object
+		food.items.push({
+			'name': foodNameInput.val(),
+			'calories': foodCaloriesInput.val()
+		});
+
+		// Update food
+		DB.put(food, function callback(err, doc) {
+			if (err) {
+				foodAddPopupFail.find('span').html('Error when store food ...');
+				foodAddPopupFail.popup('open', {});
+			} else {
+				// Update document revision
+				food._rev = doc.rev;
+
+				// Sort food
+				food.items.sort(function(food1, food2) {
+					return food1.name > food2.name;
+				});
+
+				// Clear values
+				foodNameInput.val('');
+				foodCaloriesInput.val('');
+
+				// Update corresponding data on success
+				updateLogSelect();
+				updateFoodItems();
+
+				// Success popup
+				foodAddPopupSuccess.popup('open', {});
+			}
+		});
+	});
+
+	calculateLink.on('click', function() {
+		if (!weightInput.val().length || !$.isNumeric(weightInput.val()) || weightInput.val() < 0) {
+			bmiPopupFail.find('span').html('Weight should be greater than 0 ...');
+			bmiPopupFail.popup('open', {});
+			return;
+		}
+
+		if (!heightInput.val().length || !$.isNumeric(heightInput.val()) || heightInput.val() < 0) {
+			bmiPopupFail.find('span').html('Height should be greater than 0 ...');
+			bmiPopupFail.popup('open', {});
+			return;
+		}
+
+		var result = {};
+		var weight = weightInput.val();
+		var height = heightInput.val();
+
+		result.index = weight / (height / 100 * height / 100);
+		if(result.index < 18.5){
+			result.message = 'You are too thin';
+			result.conclusion = false;
+		}
+		if(result.index > 18.5 && result.index < 25){
+			result.message = "You are healthy";
+			result.conclusion = true;
+		}
+		if(result.index > 25){
+			result.message = "You are overweight";
+			result.conclusion = false;
+		}
+
+		bmiSpan.html('Your BMI index is ' + result.index.toFixed(5));
+		bmiSpan.parent().removeClass('hidden');
+
+		if (result.conclusion) {
+			bmiPopupSuccess.find('span').html(result.message);
+			bmiPopupSuccess.popup('open', {});
+		} else {
+			bmiPopupFail.find('span').html(result.message);
+			bmiPopupFail.popup('open', {});
+		}
+	});
+
+	// Check if options is already in database
+	DB.get(OPTIONS_KEY, function(err, doc) {
+		if (err) {
+			console.log('There is errors getting options from PouchDB ...');
+
+			// Put default options in database
+			DB.put(OPTIONS, function callback(err, doc) {
+				if (err) {
+					console.log('There is errors storing options to PouchDB ...');
+				}
+				options = OPTIONS;
+			});
+		} else {
+			//options = doc;
+		}
+	});
+
+	// Check if logged food is already in database
+	DB.get(LOGGED_KEY, function(err, doc) {
+		if (err) {
+			console.log('There is errors getting events from PouchDB ...');
+
+			// Put default options in database
+			DB.put(LOGGED, function callback(err, doc) {
+				if (err) {
+					console.log('There is errors storing options to PouchDB ...');
+				}
+				logged = LOGGED;
+			});
+		} else {
+			logged = doc;
+
+			// Sort logged food
+			logged.items.sort(function(logged1, logged2) {
+				return moment(logged1.dateTime).isBefore(moment(logged2.dateTime));
+			});
+
+			updateLogItems();
+		}
+	});
+
+	// Check if database food is already in database
+	DB.get(FOOD_KEY, function(err, doc) {
+		if (err) {
+			console.log('There is errors getting events from PouchDB ...');
+
+			// Put default options in database
+			DB.put(FOOD, function callback(err, doc) {
+				if (err) {
+					console.log('There is errors storing options to PouchDB ...');
+				}
+				food = FOOD;
+			});
+		} else {
+			food = doc;
+
+			// Sort food
+			food.items.sort(function(food1, food2) {
+				return food1.name > food2.name;
+			});
+
+			updateLogSelect();
+			updateFoodItems();
+		}
+	});
+
+	/*
 	var saveOptionsLink = $('#saveOptionsLink');
 	var gymTimeInput = $('#gymTimeInput');
 	var drinkTimeInput = $('#drinkTimeInput');
@@ -57,18 +354,6 @@ $(document).ready(function() {
 
 	var eventsHolder = $('#eventsHolder');
 	var eventTemplate = $('#eventTemplate');
-
-	var toEvent = function() {
-		/*
-		if (events.items && events.items.length) {
-			eventName.find('.name').html(events.items[0].name);
-			eventTimer.find('.date').html(moment(events.items[0].date).format('MM-DD-YYYY HH:mm:ss'));
-		} else {
-			eventName.find('.name').html('Nothing is scheduled yet ...');
-			eventTimer.find('.date').html('--:--:--');
-		}
-		*/
-	};
 
 	var toDo = function() {
 		var timersPopupMessage = timersPopup.find('span');
@@ -176,69 +461,6 @@ $(document).ready(function() {
 		}
 	};
 
-	// Check if options is already in database
-	DB.get(OPTIONS_KEY, function(err, doc) {
-		if (err) {
-			console.log('There is errors getting options from PouchDB ...');
-
-			// Put default options in database
-			DB.put(OPTIONS, function callback(err, doc) {
-				if (err) {
-					console.log('There is errors storing options to PouchDB ...');
-				}
-				options = OPTIONS;
-			});
-		} else {
-			options = doc;
-
-			// Trigger options update
-			updateOptions();
-
-			// Trigger options update on page change
-			$(document).on('pagecontainerchange', function(event, ui) {
-				updateOptions();
-			});
-
-			// Start timers
-			count();
-		}
-	});
-
-	// Check if options is already in database
-	DB.get(EVENTS_KEY, function(err, doc) {
-		if (err) {
-			console.log('There is errors getting events from PouchDB ...');
-
-			// Put default options in database
-			DB.put(EVENTS, function callback(err, doc) {
-				if (err) {
-					console.log('There is errors storing options to PouchDB ...');
-				}
-				events = EVENTS;
-			});
-		} else {
-			events = doc;
-
-			// Filter events (remove events in the past)
-			events.items = events.items.filter(function(event) {
-				return moment(event.date).isAfter(moment());
-			});
-
-			// Sort events
-			events.items.sort(function(event1, event2) {
-				return moment(event1.date).isAfter(moment(event2.date));
-			});
-
-			// Trigger events update
-			updateEvens();
-
-			// Trigger events update on page change
-			$(document).on('pagecontainerchange', function(event, ui) {
-				updateEvens();
-			});
-		}
-	});
-
 	// Save entered data on click
 	saveOptionsLink.on('click', function() {
 		if (!moment(gymTimeInput.val(), 'HH:mm:ss', true).isValid()) {
@@ -338,4 +560,5 @@ $(document).ready(function() {
 	$(document).find('.timers').on('popupafterclose', function() {
 		count();
 	});
+	*/
 });
