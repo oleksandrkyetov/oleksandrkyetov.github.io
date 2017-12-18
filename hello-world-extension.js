369747a15d1fe6301e92ae04e41ce489110849ea
Oleksandr Kyetov <okyetov@cogniance.com>
2017-12-18 16:27:59 +0200

    Changed script

--- hello-world-extension.js
+++ hello-world-extension.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+var div = document.createElement('div');
+div.style.position = 'fixed';
+div.style.fontSize = 'xx-large';
+div.style.top = 0;
+div.style.right = 0;
+div.textContent = 'Hello World';
+document.body.appendChild(div);
+
+alert('Hello World');
