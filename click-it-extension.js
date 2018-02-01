5e50fb1128197329d39d004b0bb2bf9f79053bcf
Oleksandr Kyetov <okyetov@cogniance.com>
2018-02-01 16:42:06 +0200

    Test

--- click-it-extension.js
+++
@@ -1,7 +0,0 @@
-var button = document.createElement('button');
-button.style.position = 'fixed';
-button.style.fontSize = 'xx-large';
-button.style.top = 0;
-button.style.left = 0;
-button.textContent = 'Click It';
-document.body.appendChild(button);
--- hello-world-extension.js
+++
@@ -1,8 +0,0 @@
-var div = document.createElement('div');
-div.style.position = 'fixed';
-div.style.fontSize = 'xx-large';
-div.style.color = 'red';
-div.style.top = 0;
-div.style.right = 0;
-div.textContent = 'Hello World';
-document.body.appendChild(div);
